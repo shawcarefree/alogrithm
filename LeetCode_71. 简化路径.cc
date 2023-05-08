667e896777c96c74bb749b5d5605ad774fcc14bb
shawcarefree <95915054+shawcarefree@users.noreply.github.com>
2023-05-08 21:41:27 +0800

    71. 简化路径

---
+++ LeetCode_71. 简化路径.cc
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        deque<string> st;
+        int n = path.size(), i = 0;
+        while(i < n)
+        {
+            if(i < n && path[i] == '/') 
+            {
+                i++;
+            }
+            else
+            {
+                string temp;
+                while(i < n && path[i] != '/') temp += path[i++];
+                if(temp == ".." && !st.empty()) st.pop_back();
+                else if(temp != ".." && temp != ".") st.push_back(temp); 
+            }
+        }
+        string res = "/";
+        while(!st.empty())
+        {
+            res += st.front() + "/";
+            st.pop_front();
+        }
+        if(res.size() > 1)
+        {
+            res.pop_back();
+        }
+        
+        return res;
+    }
+};
