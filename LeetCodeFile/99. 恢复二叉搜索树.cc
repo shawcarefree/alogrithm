a304d73588d8aa9cae2bb5e7a4932d958f91fcc6
shawcarefree <95915054+shawcarefree@users.noreply.github.com>
2023-07-31 19:18:37 +0800

    99. 恢复二叉搜索树(no)

---
+++ LeetCodeFile/99. 恢复二叉搜索树.cc
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        stack<TreeNode*> stk;
+        TreeNode* x = nullptr;
+        TreeNode* y = nullptr;
+        TreeNode* pred = nullptr;
+
+        while (!stk.empty() || root != nullptr) {
+            while (root != nullptr) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            if (pred != nullptr && root->val < pred->val) {
+                y = root;
+                if (x == nullptr) {
+                    x = pred;
+                }
+                else break;
+            }
+            pred = root;
+            root = root->right;
+        }
+
+        swap(x->val, y->val);
+    }
+};
