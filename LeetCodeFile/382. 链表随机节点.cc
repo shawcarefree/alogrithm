6c3f906df10ead96df2e391fa4ce245428ec4f7b
shawcarefree <95915054+shawcarefree@users.noreply.github.com>
2023-08-02 20:08:33 +0800

    382. 链表随机节点(no)

---
+++ LeetCodeFile/382. 链表随机节点.cc
@@ -0,0 +1,15 @@
+class Solution {
+    vector<int> arr;
+
+public:
+    Solution(ListNode *head) {
+        while (head) {
+            arr.emplace_back(head->val);
+            head = head->next;
+        }
+    }
+
+    int getRandom() {
+        return arr[rand() % arr.size()];
+    }
+};
