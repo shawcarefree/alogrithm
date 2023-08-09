6540c3ea8d60a4f8ce12bcbeb2a72e6487ca1087
shawcarefree <95915054+shawcarefree@users.noreply.github.com>
2023-08-09 21:08:34 +0800

    1281. 整数的各位积和之差(no)

---
+++ LeetCodeFile/1281. 整数的各位积和之差.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int m = 1, s = 0;
+        while (n) {
+            int x = n % 10;
+            n /= 10;
+            m *= x;
+            s += x;
+        }
+        return m - s;
+    }
+};
