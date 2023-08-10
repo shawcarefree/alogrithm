280bc1c301f0ce4493b06eb3b09cc6719e5c7f41
shawcarefree <95915054+shawcarefree@users.noreply.github.com>
2023-08-10 21:31:11 +0800

    58. 最后一个单词的长度(no)

---
+++ LeetCodeFile/58. 最后一个单词的长度.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int index = s.size() - 1;
+
+        while (s[index] == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s[index] != ' ') {
+            wordLength++;
+            index--;
+        }
+
+        return wordLength;
+    }
+};
