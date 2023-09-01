18b4cc17ade542fe670a7240c4cbb4efdcd5b28f
shawcarefree <95915054+shawcarefree@users.noreply.github.com>
2023-09-01 21:05:22 +0800

    230. 二叉搜索树中第K小的元素(no)

---
+++ LeetCodeFile/230. 二叉搜索树中第K小的元素.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode *> stack;
+        while (root != nullptr || stack.size() > 0) {
+            while (root != nullptr) {
+                stack.push(root);
+                root = root->left;
+            }
+            root = stack.top();
+            stack.pop();
+            --k;
+            if (k == 0) {
+                break;
+            }
+            root = root->right;
+        }
+        return root->val;
+    }
+};
